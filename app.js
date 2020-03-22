b07fee94d65c5940c6ddbfdd279d638738b4a51b
Sarah Rainbow <sarah.rainbow@bbc.co.uk>
2020-03-22 16:30:37 +0000

    adding things

---
+++ app.js
@@ -0,0 +1,80 @@
+var questionsAndAnswers = {
+    "How many children do you have": 4,
+    "What was Edward's first girlfriend called": 'chelsea',
+    "Who was the heaviest baby when they were born (James, Charles, Sarah or Edward)": "edward",
+    'Who is the heaviest NOW': 'james',
+    'Who was the naughtiest as a child?': 'charles',
+    'Who was the naughtiest as a teenager?': 'sarah',
+    "What was Sarah's favourite subject at secondary school": 'music',
+    "What was James' favourite subject at secondary school – maths, science, english or PE?": 'science',
+    "Who is this <br><img src='./images/baby-s.JPG' />": 'sarah',
+    "Who was Charles' best friend growing up (first and last name)" : 'ed moore',
+    "James' best friend": 'nick moore',
+    "Sarah's childhood best friend" : 'katie hall',
+    "And Edward's childhood best friend?": "Ben Spurr",
+    'And finally, who is your favourite child?': 'sarah'
+}
+
+var questions = Object.keys(questionsAndAnswers);
+var answers = Object.values(questionsAndAnswers); 
+var questionCount = 0;
+var correctAnswerCount = 0;
+
+askQuestion();
+
+function askQuestion() {
+    if (questionCount === questions.length){
+        document.getElementById("questionArea").style.display = 'none';
+        document.getElementById("result").style.display = 'none';
+    }
+    else {
+        var question = document.getElementById("question");
+        question.innerHTML = `${questions[questionCount]}?`;
+        questionCount++;
+        document.getElementById("questionCount").innerHTML = questionCount;
+
+    }
+
+}
+
+function checkAnswer() {
+    const motherAnswer = document.getElementById('answer').value;
+    const actualAnswer = answers[questionCount - 1];
+    const result = motherAnswer.toLowerCase() == actualAnswer ? "correct" : "incorrect";
+    displayResults(result);
+    document.getElementById('answer').value = '';
+}
+
+function correctAnswer(){
+    correctAnswerCount ++;
+}
+
+function displayResults(result) {
+    if (questionCount == questions.length) {
+        finalScore();
+    }
+    const resultDisplay = document.getElementById('result');
+    var message = '';
+    var correctAnswerResponses = ['Correct! Keep up the good work!', 'Nice work! You got this!', "Bingo! Great mothering!", "You got it right, getting those mother points in the bank!"];
+    var wrongAnswerResponses = ['Eh oh, not quite right…', 'Unlucky…', "D'oh!"];
+
+    let randomNumber = Math.floor(Math.random() * correctAnswerResponses.length);
+    if (result == 'correct'){
+        message = `✅ ${correctAnswerResponses[randomNumber]}`;
+        correctAnswerCount++;
+    } 
+    else {
+        message = `😭 ${wrongAnswerResponses[randomNumber]} The answer was ${answers[questionCount -1]}!`;
+    }
+    resultDisplay.innerHTML = message;
+
+}
+
+function finalScore() {
+    const scoreElement = document.getElementById('final-score');
+    scoreElement.style.display = 'block';
+    var finalScoreMessage = correctAnswerCount > 7 ? 'You are a wonderful mother, congratulations!!' : 'You are a below-par mother, but we still love you!';
+    scoreElement.innerHTML = `${finalScoreMessage} You got ${correctAnswerCount}/${questionCount}`;
+}
+
+
